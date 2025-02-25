20cbe4c58754f92057b730caee8daf70485ed0f2
pablofierrovallejos <pablofierrovallejos@gmail.com>
2025-02-25 14:49:19 -0300

    add swagger dependency

--- src/main/java/com/pablo/fierro/api_example/controller/controllerEcho.java
+++ src/main/java/com/pablo/fierro/api_example/controller/controllerEcho.java
@@ -2,6 +2,7 @@ package com.pablo.fierro.api_example.controller;
 
 import com.pablo.fierro.api_example.dto.cliente;
 import com.pablo.fierro.api_example.generator.BarcodeGenerator;
+import com.pablo.fierro.api_example.generator.MultipartImage;
 import com.pablo.fierro.api_example.service.IQrGen;
 import net.sourceforge.barbecue.Barcode;
 import net.sourceforge.barbecue.BarcodeException;
@@ -10,10 +11,12 @@ import net.sourceforge.barbecue.BarcodeImageHandler;
 import net.sourceforge.barbecue.output.OutputException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import static org.springframework.http.HttpStatus.OK;
@@ -22,6 +25,7 @@ import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
@@ -47,9 +51,19 @@ public class controllerEcho {
         return cli;
     }
 
-    @GetMapping("/qr")
-    BufferedImage qr() throws OutputException, BarcodeException {
-        return qrGen.generateCode128BarCodeImage("Pablo Fierro");
+    @GetMapping("/qr/{sTexto}")
+    MultipartImage qr(@PathVariable("sTexto") String sTexto) throws OutputException, BarcodeException, IOException {
+        BufferedImage bufferedImage = qrGen.generateQrCode(sTexto, 350, 350);
+
+        File codigoQR = new File("codigo-qr.jpg");
+        ImageIO.write(bufferedImage, "jpg", codigoQR);
+
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write( bufferedImage, "jpg", baos );
+        baos.flush();
+
+        return new MultipartImage(baos.toByteArray(), "imageName.jpg", "baos", MediaType.MULTIPART_FORM_DATA.toString(), baos.size());
     }
 
     @GetMapping("/qr2disk")
@@ -66,15 +80,21 @@ public class controllerEcho {
         ImageIO.write(bufferedImage, "jpg", codigo128);
     }
 
-    @GetMapping("/pdf417b")
-    void pdf417() throws BarcodeException, OutputException, IOException {
-        final Barcode barcode = BarcodeFactory.createPDF417("Pablo Fierro Vallejofdgdfgdfgdfgdfgdfgdffffffffffffffffffffffffffffffffffffffffffffffffs");
+    @GetMapping("/pdf417b/{sTexto}")
+    MultipartFile pdf417(@PathVariable("sTexto") String sTexto) throws BarcodeException, OutputException, IOException {
+        final Barcode barcode = BarcodeFactory.createPDF417(sTexto);
         barcode.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
         System.out.println("generatePDF417BarcodeImage: " + "barcodeText");
         BufferedImage bufferedImage = BarcodeImageHandler.getImage(barcode);
 
         File pdf417 = new File("codigo-pdf417.jpg");
         ImageIO.write(bufferedImage, "jpg", pdf417);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write( bufferedImage, "jpg", baos );
+        baos.flush();
+
+        return new MultipartImage(baos.toByteArray(), "imageName.jpg", "baos", MediaType.MULTIPART_FORM_DATA.toString(), baos.size());
     }
 
 
